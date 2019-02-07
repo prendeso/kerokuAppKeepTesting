219226439a7f9ec85873dbf572c6cfe71eda7d5b
prendeso <orlando.prendesm@ufl.edu>
2019-02-07 14:48:34 -0500

    Update listingFactory.js

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,11 +1,11 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('https://afternoon-stream-97491.herokuapp.com/api/listings');
+      return $http.get('https://bootcamp5.herokuapp.com/api/listings');
     },
 	
 	create: function(listing) {
-	  return $http.post('https://afternoon-stream-97491.herokuapp.com/api/listings', listing);
+	  return $http.post('https://bootcamp5.herokuapp.com/api/listings', listing);
     }, 
 
     delete: function(id) {
@@ -13,7 +13,7 @@ angular.module('listings', []).factory('Listings', function($http) {
         return result of HTTP delete method
        */
 	   //return $http.delete('http://localhost:8080/api/listings/', id);
-        return $http.delete('https://afternoon-stream-97491.herokuapp.com/api/listings' + id, id );
+        return $http.delete('https://bootcamp5.herokuapp.com/api/listings' + id, id );
 
     }
   };
