14499283f3d415d04de923d65c5e6209760c526e
Orlando Prendes <orlando.prendesm@ufl.edu>
2019-02-07 15:06:00 -0500

    Update listingFactory.js

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,6 +1,7 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
+      return $http.get('http://localhost:8080/api/listings');
     },
 	
 	create: function(listing) {
